2b14107a30e638ea80e5d53744060d29d258549b
Jamunabai Prakash <saigalprakash@gmail.com>
2018-05-02 12:51:50 -0400

    added put routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,33 @@ root 'home#landingpage'
   get 'home/logoutpage'
   get 'users/profile'
 
-  get    '/welcome',   to: 'home#landingpage'
+    get    '/welcome',   to: 'home#landingpage'
     get     '/center',   to:  'center#users'
     get    '/about',     to: 'home#about'
     get    '/contact',   to: 'home#contact'
     get    '/logout',    to: 'home#logoutpage'
+
     get    '/jobs',      to:  'posts#jobs'
     post   '/jobs',      to:  'posts#jobs'
+    put   '/jobs',      to:  'posts#jobs'
+
     get    '/meals',      to:  'posts#meals'
     post   '/meals',      to:  'posts#meals'
+    put   '/meals',      to:  'posts#meals'
+
     get    '/shelters',      to:  'posts#shelters'
     post   '/shelters',      to:  'posts#shelters'
+    put   '/shelters',      to:  'posts#shelters'
+
     get    '/supplies',      to:  'posts#supplies'
     post   '/supplies',      to:  'posts#supplies'
+    put   '/supplies',      to:  'posts#supplies'
+
 
   devise_for :users
 
   resources :center
-  
+
   resources :post
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
